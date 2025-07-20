613d3ab638ea9a45d8110b9090cfe0c5d27fa046
Mesrop Hakobyan <mesrop.hakobyan@web.de>
2025-07-20 16:12:41 +0000

    changing

--- test_docker/urls.py
+++ test_docker/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth',include('rest_framework.urls')),
     path('api/',include('person_date.api.urls'))
-]
+] + staticfiles_urlpatterns()
+
