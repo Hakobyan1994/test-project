3e7683ce4268ae02df332de894df3f08b6d177bd
Hakobyan1994 <mesrop.hakobyan@web.de>
2025-08-25 15:13:42 +0200

    added admin panel

--- person_date/admin.py
+++ person_date/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import PersonDate
 # Register your models here.
+
+
+
+class PersonDateAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "city","country","index"]
+    list_filter = ("city", "country", "index")
+    search_fields = ("first_name", "last_name", "city", "country", "index")
+    ordering = ("last_name", "first_name")
+    list_display_links = ("first_name", "last_name")
+
+
+admin.site.register(PersonDate,PersonDateAdmin)
